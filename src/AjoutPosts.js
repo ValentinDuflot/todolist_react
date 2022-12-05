35890a90398540f57f00a532c6db5acde8075cb3
Valentin Duflot <duflot.valentin@gmail.com>
2022-12-05 09:59:42 +0100

    <Feat> Le formulaire d'ajout de post est initialisé à la date/heure actuelle

--- src/AjoutPosts.js
+++ src/AjoutPosts.js
@@ -13,12 +13,41 @@ export default function AjoutPosts() {
     // permet de switch la visibilité du formulaire
     let [ajoutToggle, toggleAjout] = useState(false);
 
+    // pour obtenir la date et heure actuelle
+    let today = new Date();
+    
+    // on cherche à obtenir la date et l'heure actuelle
+    let dateTime = today.getFullYear()+"-"; 
+    // correction du mois : nécessite un affichage à deux chiffres même pour les mois avant octobre (10e mois...)
+    today.getMonth()+1>=10 ? 
+        dateTime += today.getMonth()+1 :
+        dateTime += '0'+(today.getMonth()+1)
+    dateTime += "-";
+    // correction du jour
+    today.getDate()>=10 ? 
+        dateTime += today.getDate() :
+        dateTime += '0'+(today.getDate())
+
+    dateTime += "T";
+    
+    // correction de l'heure
+    today.getHours()>=10 ? 
+        dateTime += today.getHours() :
+        dateTime += '0'+(today.getHours())
+    dateTime += ":";
+    // correction de la minute
+    today.getMinutes()>=10 ? 
+        dateTime += today.getMinutes() :
+        dateTime += '0'+(today.getMinutes())
+
+
     // variables d'état
     let [titre, setTitre] = useState("Titre");
     let [contenu, setContenu] = useState("Contenu");
-    let [dateLimite, setDateLimite] = useState("2022-06-12T19:30");
+    let [dateLimite, setDateLimite] = useState(dateTime); // intilisé à la date/heure actuelle
     let [couleur, setCouleur] = useState("#f6b73c");
 
+ 
     // fonction d'ajout de post, invoquée lors de l'envoi du formulaire
     const ajoutPost = () => {
         toggleAjout(false);
